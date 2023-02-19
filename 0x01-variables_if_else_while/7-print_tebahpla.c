bcee690ed0aa2c204d9124c239b53fef2ee045a4
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-02-19 14:21:05 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Main Function
+ * Return: Return 0 if successful
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+
+	int i;
+
+	for (i = (strlen(alp) - 1); i >= 0; i--)
+		putchar(alp[i]);
+
+	putchar('\n');
+
+	return (0);
+}
