441d018045b1895fd07701a73b7f1aac5f267e70
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-02-19 06:32:15 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Main Function
+ * Return: Return 0 if successful
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
+}
