b232bd30d56f1725b8c2f067ca6ca2a42ff1474c
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-02-21 20:20:21 -0800

    README.md and 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+/* more headers goes there */
+
+/**
+ * main - Main Function
+ * Return: Return 0 if successful
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char a[] = "_putchar";
+	int a_size = sizeof(a) / sizeof(a[0]);
+	int i;
+
+	for (i = 0; i < a_size; i++)
+	{
+		putchar(a[i]);
+	}
+
+	putchar('\n');
+	return (0);
+}
