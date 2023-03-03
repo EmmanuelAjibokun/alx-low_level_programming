c3e4fc80b3d128af2e8325ec1ad36f1aa687b686
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-03-02 21:00:08 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _ - copy string2 to string1
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+* Return: dest
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, new_arr[];
+
+	i = 
+	while (i >= 0)
+	{
+		new_arr[j] = a[i];
+
+		j++;
+		i--;
+	}
+}
