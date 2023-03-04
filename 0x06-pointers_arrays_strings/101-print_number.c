e167e808616ff7f399579a275b364ce6b15569eb
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-03-04 08:01:09 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_number - a function that prints an integer
+ * @n: input value
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i, length;
+	char int_str[0];
+
+	length = snprintf(NULL, 0, "%d", n);
+
+	sprintf(int_str, "%d", n);
+	for (i = 0; i < length; i++)
+	{
+		_putchar(int_str[i]);
+	}
+
+}
