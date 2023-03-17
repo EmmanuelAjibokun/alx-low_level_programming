05dc5a560c511575770d149a2a9ad7bd4dde7604
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-03-16 21:42:05 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib>
+#include <stdlib.h>
 
 /**
  * malloc_checked - Returns a pointer to the allocated memory
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * malloc_checked - Returns a pointer to the allocated memory
+ * @b: memory size
+ *
+ * Return: void
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{}
