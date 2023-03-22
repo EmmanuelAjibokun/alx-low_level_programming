7710a684a95fcd1e5fed8a3ff46fe5425a4aeb76
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-03-22 02:55:38 -0700

    a header file with defined structure for object dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - is a structure for a dog object
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+struct dog
+{
+	char name[20];
+	float age;
+	char owner[20];
+};
+
+#endif
