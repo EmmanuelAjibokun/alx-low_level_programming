6a7aade01b9b812c62453ec8a7e0d130a48e7e66
EmmanuelAjibokun <emmanuelajibokun@gmail.com>
2023-03-24 04:48:05 -0700

    Returns the index of the first value that returns true in a callback function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searchs for an integer
+ * @array: array of data
+ * @size: size of array
+ * @cmp: input function
+ * Return: i
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+}
