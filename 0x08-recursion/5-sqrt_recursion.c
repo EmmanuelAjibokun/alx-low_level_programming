f7c3e70b92d689b19e61c7725ca943e9d3ae6d37
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-08 09:15:35 -0700

    Create program to get the square root of a value

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/*
+ * _sqrt_wrapper - Returns the natural square root of a number.
+ * _sqrt_recursion - Calls the recursive function.
+ * @n: integer to get squate root
+ * @min: lower boundary
+ * @max: upper boundary
+ *
+ * Return: value of root;
+ */
+
+int _sqrt_wrapper(int n, int min, int max)
+{
+	int guess, guess_squared;
+
+	if (max < min)
+		return (-1);
+	guess = (min + max) / 2;
+	guess_squared = guess * guess;
+
+	if (guess_squared == n)
+		return (guess);
+	else if (guess_squared < n)
+		_sqrt_wrapper(n, guess + 1, max);
+	else
+		_sqrt_wrapper(n, min, guess - 1);
+}
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_wrapper(n, 1, n));
+}
