73c89a30b41baa70c88d4bc3ce2c0d53a586ef5a
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-10 04:47:57 -0700

    Multiply two arguments from command

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - multiply two arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error\n");
+
+	return (1);
+}
