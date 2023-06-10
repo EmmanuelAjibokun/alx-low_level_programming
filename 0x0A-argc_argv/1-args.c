4ef58658db5c12d2f0eb2546447715f633087a61
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-10 04:12:27 -0700

    create file that prints the number of arguments of a command

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * main - prints number of arguments passed to command
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar('0' + argc - 1);
+	(void)argv;
+	return (0);
+}
