2a57d5a60761f112fb516aeb288bb69cce4250e2
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-13 13:37:26 -0700

    allocate memory to concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * str_concat -  pointer should point to a newly allocated
+ * space in memory which contains the contents of s1,
+ * followed by the contents of s2
+ * @s1: string input
+ * @s2: string input
+ *
+ * Return: NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, str_len1, str_len2, total_len, counter;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1[0] = "";
+	if (s2 == NULL)
+		s2[0] = "";
+
+	str_len1 = strlen(s1);
+	str_len2 = strlen(s2);
+	total_len = str_len1 + str_len2;
+	counter = 0;
+	j = 0;
+
+	ptr = malloc((sizeof(char) * total_len) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < str_len; i++)
+	{
+		ptr[i] = s1[i];
+		counter++;
+	}
+
+	for (i = counter; i < total_len; i++)
+	{
+		while (j < str_len2)
+		{
+			ptr[i] = s2[j];
+			j++;
+			break;
+		}
+	}
+
+	return (ptr);
+}
