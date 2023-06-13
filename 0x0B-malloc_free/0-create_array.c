fb436533f29dbd662b856bced283b3d85a93a440
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-13 09:01:49 -0700

    function that allocates memory and fills it with a character

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * create_array - a function that creates an array of chars.
+ * @size: size of allocated memory
+ * @c: input character
+ *
+ * Return: NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	int i;
+	char *ptr[];
+
+	ptr = malloc((sizeof(c) * size) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	
+	for (i = 0; i <+ size; i++)
+		ptr[i] = c;
+	return (ptr);
+}
