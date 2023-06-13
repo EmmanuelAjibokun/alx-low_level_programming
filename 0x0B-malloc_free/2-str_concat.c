39d4c5fb339b496a641ca93ca42d05b2de56b548
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-13 13:49:37 -0700

    add empty character constant

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1[0] = '';
+		s1[0] = '\0';
 	if (s2 == NULL)
-		s2[0] = '';
+		s2[0] = '\0';
 
 	str_len1 = strlen(s1);
 	str_len2 = strlen(s2);
