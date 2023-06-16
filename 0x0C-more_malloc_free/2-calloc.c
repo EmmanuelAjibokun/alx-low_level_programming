d01731782a75a29e75ffa6d0dea67e06bb5c632e
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-15 18:10:24 -0700

    allocate memory and initialize to zero

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array of elements
+ * @nmemb: number of blocks
+ * @size: data size of each block
+ *
+ * Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, j;
+	int *ptr;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}
