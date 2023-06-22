9aa3e23a1369efd510a15f051ffda7f118ab3f2b
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-22 05:53:00 -0700

    function tha gives argument for a function pointed to

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: pointer to another function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	&f(name);
+}
