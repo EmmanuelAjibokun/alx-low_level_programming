a548eb1c7c85b499e44440805dc928b1d8a23f20
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-22 08:42:22 -0700

    fix NULL error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 /**
- * array_iterator - a function that executes a function given as 
+ * array_iterator - a function that executes a function given as
  * a parameter on each element of an array.
  * @array: array of items
  * @size: array length
@@ -16,6 +16,9 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
+
+	if (array == NULL || action == NULL)
+		exit(1);
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
