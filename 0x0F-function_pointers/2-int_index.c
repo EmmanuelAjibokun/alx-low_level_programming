addd61570b2bdfee375f7ac7ec6ffa4ad175296d
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-06-22 09:21:35 -0700

    function returns the index of the first number

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index -  a function that searches for an integer.
+ * @array: array of numbers
+ * @size: array length
+ * @cmp: pointer to a function
+ *
+ * Return: integer value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
