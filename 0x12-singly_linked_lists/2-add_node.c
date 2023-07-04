5490c3dad051b7e39f6c5f4bffe8904760423339
Emmanuel Ajibokun <emmanuelajibokun9@gmail.com>
2023-07-04 05:10:44 -0700

    function that adds new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,15 +15,21 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	char *nodelist;
+
+	nodelist = strdup(str);
+	if (nodelist == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	new->str = nodelist;
+	new->len = strlen(nodelist);
 	new->next = *head;
 	*head = new;
 
-	return ((*head)->next);
+	return (new);
 }
